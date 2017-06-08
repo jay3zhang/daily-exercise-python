4feb2fcb434df24f10c1e242696e6f9bf740f183
jay3zhang <jay_1121863910@163.com>
2017-06-08 17:06:03 +0800

    debug 一个list访问越界的问题

--- daily-algorithm/pythonds/code/quicksort.py
+++ daily-algorithm/pythonds/code/quicksort.py
@@ -36,10 +36,11 @@ def realsort(alist,start,end):
     done = False
     
     while not done:
-        while alist[leftmark] <= diffvalue and leftmark <= rightmark:
+        # leftmark与rightmark的比较要放在前面，否则会出现数组越界（先执行前面的表达式）
+        while leftmark <= rightmark and alist[leftmark] <= diffvalue:
             leftmark += 1   # 找到左侧大于对比值的位置
             
-        while alist[rightmark] >= diffvalue and rightmark >= leftmark:
+        while rightmark >= leftmark and alist[rightmark] >= diffvalue:
             rightmark -= 1  ## 找到右侧小于对比值的位置
             
         if rightmark < leftmark:    # 右标记小于左标记时，大小区分完成
@@ -57,8 +58,12 @@ def realsort(alist,start,end):
     
     return rightmark
             
-alist = [31,26,93,17,77,54,44,55,20]
-quicksort(alist)
-print(alist)
+# alist = [31,26,93,17,77,54,44,55,20]
+# quicksort(alist)
+# print(alist)
+
+test=[54,35,48,36,27,12,44,44,8,14,26,17,28]
+quicksort(test)
+print(test)
     
     
