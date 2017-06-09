16018ce6d641a90245d2cbebe9879f2d61ab0ea7
jay3zhang <jay_1121863910@163.com>
2017-06-09 20:59:18 +0800

    更新两道算法题的好解法

---
+++ daily-algorithm/count1_at1_toN.py
@@ -0,0 +1,30 @@
+﻿
+# -*- coding:utf-8 -*-
+# 1到N的整数中1出现的次数
+class Solution:
+    def NumberOf1Between1AndN_Solution(self, n):
+        # write code here
+        res=0
+        tmp=n
+        base=1
+        while tmp:
+            last=tmp%10
+            tmp=tmp/10
+            res+=tmp*base
+            if last==1:
+                res+=n%base+1
+            elif last>1:
+                res+=base
+            base*=10
+        return res
+        
+        
+def NumberOf1Between1AndN2(self, n):
+        ones, m = 0, 1
+        while m <= n:
+            if ((n // m) % 10) != 0 and ((n // m) % 10) != 1:
+                ones += (n // 10 // m + 1) * m
+            elif ((n // m) % 10) == 1:
+                ones += (n // m // 10) * m + n % m + 1
+            m *= 10
+        return ones
---
+++ daily-algorithm/partition_num.py
@@ -0,0 +1,27 @@
+﻿# 将奇数放到偶数前面
+# 保持相对顺序
+
+# 使用额外空间
+def reOrderArray(array):
+        # write code here
+        lo = []
+        le = []
+        for d in array:
+            if d % 2 == 1:
+                lo.append(d)
+            else:
+                le.append(d)
+        return lo + le
+# 不使用额外空间
+def reOrderArray(array):
+        # write code here
+        if not array:
+            return []
+        # 不需要额外空间
+       	for i in range(0,len(array)):
+            for j in range(len(array)-1,i,-1):
+                if array[j-1]%2 ==0 and array[j]%2==1:
+                    temp = array[j-1]
+                    array[j-1] = array[j]
+                    array[j] = temp
+        return array
